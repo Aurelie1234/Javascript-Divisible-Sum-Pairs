848d5ecc17fefdd0f89807b6a147ee7ac2f66e70
Aurelie1234 <115810657+Aurelie1234@users.noreply.github.com>
2023-05-05 10:04:16 +0000

    Create Javascript Divisible Sum Pairs.js

---
+++ Javascript Divisible Sum Pairs.js
@@ -0,0 +1,16 @@
+function divisibleSumPairs(n, k, ar) {
+  
+    // Write your code here
+let count = 0;
+
+
+for (let i = 0; i < ar.length ; i++){
+    for (let j = i + 1 ; j < ar.length ; j++) {
+        if( (ar[i] + ar[j])% k === 0) {
+             count++
+         }
+}
+        }
+         
+return count
+}
